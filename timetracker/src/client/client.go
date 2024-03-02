253e2cc3f4f0709b913d1ebde1c632de024daf61
Dmytro <juvenigi@gmail.com>
2024-03-02 01:46:19 +0100

    temporarily fix some of the buffering issues
    - we still have a race condition when the server wants to send a response

--- timetracker/src/client/client.go
+++ timetracker/src/client/client.go
@@ -26,48 +26,30 @@ func main() {
 }
 
 func ExampleDial(count int) {
+	//TODO: name the pipe appropriately, and add timeout.
 	conn, err := npipe.Dial(`\\.\pipe\mypipe`)
 	if err != nil {
 		log.Fatal("Error connecting to the named pipe:", err)
 	}
 
-	message := fmt.Sprintf("Hi server! Count: %d", count)
+	// define the buffer reader and writer
+	buffWriter := bufio.NewWriter(conn)
+	buffReader := bufio.NewReader(conn)
 
 	// Create a channel to receive responses from the goroutine
 	responseChan := make(chan string)
 
+	// write messages to server
+	writeMessagesToBuffer(count, buffWriter, fmt.Sprintf("Hi server! Count: %d", count))
+
+	// Flush the buffer to ensure all messages are sent
+	if err := buffWriter.Flush(); err != nil {
+		_ = conn.Close()
+		log.Fatal("Error flushing the buffer:", err)
+	}
+
 	// Goroutine for reading and buffering responses
-	go func() {
-		for {
-			response, err := bufio.NewReader(conn).ReadString('\n')
-			if err != nil {
-				log.Fatal("Error reading response from the named pipe:", err)
-			}
-
-			// Check for an empty response and close the connection
-			if response == "\n" {
-				conn.Close()
-				close(responseChan)
-				return
-			}
-
-			// Send response to the channel
-			responseChan <- response
-
-		}
-	}()
-
-	// Send messages to the server
-	for i := 0; i < count; i++ {
-		if _, err := fmt.Fprintln(conn, message); err != nil {
-			log.Fatal("Error writing to the named pipe:", err)
-		}
-	}
-
-	// send newline to the server
-	if _, err := fmt.Fprintln(conn); err != nil {
-		log.Fatal("Error writing to the named pipe:", err)
-	}
+	go handleResponses(buffReader, conn, responseChan)
 
 	// Receive and print the last response
 	// Create a variable to keep track of the maximum channel size
@@ -80,4 +62,41 @@ func ExampleDial(count int) {
 
 	// Print the last response
 	fmt.Printf("%slength: %d\n", lastResponse, counter)
+}
+
+// TODO: add a timeout to the response channel
+func handleResponses(buffReader *bufio.Reader, conn *npipe.PipeConn, outputChan chan string) {
+	for {
+		response, err := buffReader.ReadString('\n')
+		if err != nil {
+			log.Fatal("Error reading response from the named pipe:", err)
+		}
+
+		// Check for an empty response and close the connection
+		if response == "\n" {
+			_ = conn.Close()
+			close(outputChan)
+			return
+		} else {
+			// Print the response received from the server
+			fmt.Print(response)
+		}
+
+		// Send response to the channel
+		outputChan <- response
+
+	}
+}
+
+func writeMessagesToBuffer(count int, buffWriter *bufio.Writer, messageToServer string) {
+	// Send messages to the server
+	for i := 0; i < count; i++ {
+		if _, err := fmt.Fprintln(buffWriter, messageToServer); err != nil {
+			log.Fatal("Error writing to the named pipe:", err)
+		}
+	}
+	// send newline to the server
+	if _, err := fmt.Fprintln(buffWriter); err != nil {
+		log.Fatal("Error writing to the named pipe:", err)
+	}
 }
--- timetracker/src/daemon/daemon.go
+++ timetracker/src/daemon/daemon.go
@@ -15,11 +15,13 @@ func main() {
 
 // Use Listen to start a server, and accept connections with Accept().
 func ExampleListen() {
+	log.Println("Starting server...")
 	ln, err := npipe.Listen(`\\.\pipe\mypipe`)
 	if err != nil {
 		// handle error
-		log.Fatal("something went wrong")
+		log.Fatal("something went wrong", err.Error())
 	}
+	log.Println("Server started!")
 
 	defer ln.Close()
 
@@ -27,6 +29,7 @@ func ExampleListen() {
 
 	for {
 		conn, err := ln.Accept()
+		log.Println("Connection accepted!")
 		if err != nil {
 			// handle error
 			log.Println("some error with connection")
@@ -35,39 +38,53 @@ func ExampleListen() {
 
 		// handle connection like any other net.Conn
 		go func(conn net.Conn) {
+			defer log.Println("Connection closed!")
 			defer conn.Close()
-			defer fmt.Println()
 
-			r := bufio.NewReader(conn)
+			reader := bufio.NewReader(conn)
+			writer := bufio.NewWriter(conn)
 
 			for {
-				msg, err := r.ReadString('\n')
+				msg, err := reader.ReadString('\n')
 				if err != nil {
 					// handle error or end of connection
+					log.Println("connection ended or read fail occurred.")
 					return
 				}
 
+				// Print the message received from the client
 				fmt.Print(msg)
 
-				// If an empty string is received, emit an empty response
-				if msg == "\n" {
-					if _, err := fmt.Fprintln(conn, ""); err != nil {
-						// handle error writing response
-						return
-					}
-
-					// Close the connection after responding to the newline
-					conn.Close()
+				if handleMessage(msg, writer, &messageCount, err) {
 					return
-				} else {
-					messageCount++
-					response := fmt.Sprintf("I've received %d messages so far\n", messageCount)
-					if _, err := fmt.Fprintln(conn, response); err != nil {
-						// handle error writing response
-						return
-					}
 				}
 			}
 		}(conn)
 	}
+}
+
+func handleMessage(msg string, writer *bufio.Writer, messageCount *int, err error) bool {
+	// If an empty string is received, emit an empty response
+	if msg == "\n" {
+		if _, err := fmt.Fprintln(writer, fmt.Sprintf("Server: I've received %d messages so far\n", *messageCount)); err != nil {
+			// handle error writing response
+			log.Println("Error writing response to the named pipe:", err.Error())
+		}
+		if _, err := fmt.Fprintln(writer, ""); err != nil {
+			// handle error writing response
+			log.Println("Error writing newline response to the named pipe:", err.Error())
+		}
+		err := writer.Flush()
+		if err != nil {
+			log.Println("Error while flushing")
+		}
+		return true // break out of the loop
+	} else {
+		*messageCount = *messageCount + 1
+
+		if err != nil {
+			log.Println("Error writing to the named pipe:", err.Error())
+		}
+	}
+	return false
 }
